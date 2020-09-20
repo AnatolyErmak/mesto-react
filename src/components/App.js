954dfdb5859b4c40fe80070b69fac1b823c5f3e9
Anatoly Ermakov <Pr0f3ssAr@yandex.ru>
2020-09-20 20:03:19 +0300

    update PopupWithForm in App

--- src/components/App.js
+++ src/components/App.js
@@ -67,51 +67,47 @@ function App() {
       onClose = {closeAllPopups}
       />
       <Footer/>
-      <PopupWithForm name = "popup_avatar" isOpen = {isAvatarOpen} title = "Обновить аватар"
-      children = {
-        <>
+      <PopupWithForm name = "popup_avatar" isOpen = {isAvatarOpen} title = "Обновить аватар" onClose = {closeAllPopups}>
+        {/* children */}
         <input className="popup__field" id="avatar-url-input" name="avatar" type="url" placeholder="Введите url" required></input>
         <span className="popup__span-error" id="avatar-url-input-error"></span>
-        </>
-      } onClose = {closeAllPopups}
-      />
-      <PopupWithForm name = "popup_profile" isOpen = {isProfileOpen} title = "Редактировать профиль"
-      children = {
-        <>
+
+      </PopupWithForm>
+   
+      <PopupWithForm name = "popup_profile" isOpen = {isProfileOpen} title = "Редактировать профиль" onClose = {closeAllPopups}>
+          {/* children */}
         <input className="popup__field popup__field_name" type="text" id="profile__input_name"
          placeholder="Имя" required minLength="2" maxLength="40" name = "name"></input>
         <span className="popup__span-error" id="profile__input_name-error"></span>
         <input className="popup__field popup__field_about" type="text" name = "job" placeholder="О себе"
          required minLength="2" maxLength="200" id="profile__input_about"></input>
         <span className="popup__span-error" id="profile__input_about-error" ></span>
-        </>
-      } onClose = {closeAllPopups}
-      />
+
+      </PopupWithForm>
+
       <PopupWithForm name = "popup_card" isOpen = {isAddCardOpen} title = "Новое место"
-      children = {
-        <>
-        <input className="popup__field popup__field_name" id="cardName" type="text" name = "name"
+       onClose = {closeAllPopups}>
+
+         {/* children */}
+
+         <input className="popup__field popup__field_name" id="cardName" type="text" name = "name"
            placeholder="Название" value="" required minLength="1" maxLength="30"></input>
          <span className="popup__span-error" id="cardName-error"></span>
          <input className="popup__field popup__field_about" id="cardUrl" type="url"
            name="link" placeholder="Ссылка на картинку" value="" required></input>
          <span className="popup__span-error" id="cardUrl-error"></span>
-        </>
-      } onClose = {closeAllPopups}
-      />
-      <PopupWithForm name = "popup_delete" isOpen = {deleteIsOpen} title = "Вы уверены?"
-      children = {
-        <>
-        <button className="popup__button popup__button_delete" type="submit" aria-label="Да">Да</button>
-        </>
-      } onClose = {closeAllPopups}
-      />
+       </PopupWithForm>
+
+      <PopupWithForm name = "popup_delete" isOpen = {deleteIsOpen} title = "Вы уверены?" onClose = {closeAllPopups}>
+            {/* children */}
+          <button className="popup__button popup__button_delete" type="submit" aria-label="Да">Да</button>
+      </PopupWithForm>
+
       <ImagePopup
       isOpen = {selectedCard.isImageOpen}
       onClose = {closeAllPopups}
       name = {selectedCard.name}
       link =  {selectedCard.link}
-
       />
     </div>          
   );
